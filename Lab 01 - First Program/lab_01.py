d07b2901d7594692d02d38f37940c5bc259fc837
Sammy Cardinal <sammy.cardinal@my.simpson.edu>
2021-09-03 11:44:20 -0500

    Adjusted grammar

--- Lab 01 - First Program/lab_01.py
+++ Lab 01 - First Program/lab_01.py
@@ -1,5 +1,4 @@
-print("I like dogs")
+print("I like dogs.")
 print("Dogs are amazing because:")
-print("""They are loyal
-They are friendly
-They are normally soft or fluffy""")
+print("They are loyal, they are friendly, and they are normally soft or fluffy.")
+print("This is why everyone should own a dog.")
