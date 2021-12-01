33a13b7e584946cd57708fd97c9c0890fd4a2ade
Sammy Cardinal <sammy.cardinal@my.simpson.edu>
2021-12-01 12:15:48 -0600

    adjusted Lab 11 pt 1

--- Lab 10 - Spell Check/lab_10.py
+++ Lab 10 - Spell Check/lab_10.py
@@ -4,3 +4,57 @@ import re
 # a list of words in the line.
 def split_line(line):
     return re.findall('[A-Za-z]+(?:\'[A-Za-z]+)?',line)
+
+def read_in_file(file_name):
+    """ Read in lines from a file """
+
+    # Open the file for reading, and store a pointer to it in the new
+    # variable "file"
+    my_file = open(file_name)
+
+    # Create an empty list to store our names
+    name_list = []
+
+    # Loop through each line in the file like a list
+    for line in my_file:
+        # Remove any line feed, carriage returns or spaces at the end of the line
+        line = line.strip()
+
+        # Add the name to the list
+        dictionary_list.append(line)
+
+    my_file.close()
+
+    return dictionary_list
+
+
+def linear_search(key, dictionary_list):
+    """ Linear search """
+
+    # Start at the beginning of the list
+    current_list_position = 0
+
+    # Loop until you reach the end of the list, or the value at the
+    # current position is equal to the key
+    while current_list_position < len(dictionary_list) and dictionary_list[current_list_position] != key:
+
+        # Advance to the next item in the list
+        current_list_position += 1
+
+    return current_list_position
+
+
+def main():
+
+    key = word
+    word = dictionary
+    name_list = read_in_file("dictionary.txt")
+    list_position = linear_search(key, dictionary_list)
+
+    if list_position < len(name_list):
+        print("The word", key, "is at position", list_position)
+    else:
+        print("The name", key, "was not in the list.")
+
+
+main()
--- Lab 11 - Array-Backed Grids/lab_11_part_1.py
+++ Lab 11 - Array-Backed Grids/lab_11_part_1.py
@@ -82,6 +82,11 @@ class MyGame(arcade.Window):
 
         # Make sure we are on-grid. It is possible to click in the upper right
         # corner in the margin and go to a grid location that doesn't exist
+        if self.grid[row][column] == 0:
+            self.grid[row][column] = 1
+        else:
+            self.grid[row][column] = 0
+
         if row + 1 < ROW_COUNT:
 
             if self.grid[row + 1][column] == 0:
@@ -95,7 +100,7 @@ class MyGame(arcade.Window):
             if self.grid[row - 1][column] == 0:
                 self.grid[row  - 1][column] = 1
             else:
-                self.grid[row - 1][column + 1] = 0
+                self.grid[row - 1][column] = 0
 
         if column + 1 < COLUMN_COUNT:
 
@@ -105,7 +110,7 @@ class MyGame(arcade.Window):
             else:
                 self.grid[row][column + 1] = 0
 
-        if column < 0:
+        if column - 1 > 0:
 
             # Flip the location between 1 and 0.
             if self.grid[row][column - 1] == 0:
