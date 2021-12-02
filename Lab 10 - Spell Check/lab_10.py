42f6eff138d2db9e5a5676c97800ee8a60e009c6
Sammy Cardinal <sammy.cardinal@my.simpson.edu>
2021-12-02 10:03:49 -0600

    Lab 10 adjustments

--- Lab 10 - Spell Check/lab_10.py
+++ Lab 10 - Spell Check/lab_10.py
@@ -5,56 +5,52 @@ import re
 def split_line(line):
     return re.findall('[A-Za-z]+(?:\'[A-Za-z]+)?',line)
 
-def read_in_file(file_name):
-    """ Read in lines from a file """
-
-    # Open the file for reading, and store a pointer to it in the new
-    # variable "file"
-    my_file = open(file_name)
-
-    # Create an empty list to store our names
-    name_list = []
-
-    # Loop through each line in the file like a list
+def main():
+    # Dictionary
+    my_file = open("dictionary.txt")
+    dictionary_list = []
     for line in my_file:
-        # Remove any line feed, carriage returns or spaces at the end of the line
         line = line.strip()
-
-        # Add the name to the list
         dictionary_list.append(line)
+    my_file.close()
+    print("--- Linear Search ---")
 
+    # Alice in WonderLand
+    line_number = 0
+    my_file = open("AliceInWonderLand200.txt")
+    for line in my_file:
+        line_number += 1
+        word_list = split_line(line)
+        for word in word_list:
+            current_list_position = 0
+            while current_list_position < len(dictionary_list) and dictionary_list[current_list_position] != word.upper():
+                current_list_position += 1
+            if current_list_position == len(dictionary_list):
+                print("Line", line_number, "possible misspelled word:", word)
     my_file.close()
 
-    return dictionary_list
-
-
-def linear_search(key, dictionary_list):
-    """ Linear search """
-
-    # Start at the beginning of the list
-    current_list_position = 0
-
-    # Loop until you reach the end of the list, or the value at the
-    # current position is equal to the key
-    while current_list_position < len(dictionary_list) and dictionary_list[current_list_position] != key:
-
-        # Advance to the next item in the list
-        current_list_position += 1
-
-    return current_list_position
-
-
-def main():
-
-    key = word
-    word = dictionary
-    name_list = read_in_file("dictionary.txt")
-    list_position = linear_search(key, dictionary_list)
-
-    if list_position < len(name_list):
-        print("The word", key, "is at position", list_position)
-    else:
-        print("The name", key, "was not in the list.")
+    # Binary Search
+    print("--- Binary Search ---")
+    line_number = 0
+    my_file = open("AliceInWonderLand200.txt")
+    for line in my_file:
+        line_number += 1
+        word_list = split_line(line)
+        for word in word_list:
+            lower_bound = 0
+            upper_bound = len(dictionary_list) + 1
+            found = False
+            while lower_bound <= upper_bound and not found:
+                middle_pos = (lower_bound + upper_bound) // 2
+                if dictionary_list[middle_pos] < word.upper():
+                    lower_bound = middle_pos + 1
+                elif dictionary_list[middle_pos] > word.upper():
+                    upper_bound = middle_pos - 1
+                else:
+                    found = True
+            if not found:
+                print("Line", line_number, "possible misspelled word:", word)
+    my_file.close()
 
 
 main()
